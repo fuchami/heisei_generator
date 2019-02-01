272add47ed047fceb79a1caf5500a24441a85a20
fuchami <famichiki.yuuki@gmail.com>
2019-02-01 19:14:19 +0900

    heroku導入デプロイ前

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 # coding:utf-8
 
 from flask import Flask, render_template, request, logging, Response, redirect, flash
+import tw_generate
 
 # Flaskの起動 -インスタンス作成-
 app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    tw = 'これはテストツイートです。'
-
     if request.method == 'POST':
-        return render_template('index.html', gen_tweet=tw)
+        word = tw_generate.gen_tweet()
+        tweet = "平成最後の" + word 
+
+        return render_template('index.html', gen_tweet=tweet)
     else:
         return render_template('index.html')
 
